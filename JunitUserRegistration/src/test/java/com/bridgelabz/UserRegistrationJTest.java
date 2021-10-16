85240d670775b05c2bd02b531e3ced6c568f715a
prashant169 <prashantphad2018@gmail.com>
2021-10-16 01:07:58 -0800

    Added:Program to enter valid first name

---
+++ JunitUserRegistration/src/test/java/com/bridgelabz/UserRegistrationJTest.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserRegistrationJTest {
+	UserRegistrationJ userRegistration = new UserRegistrationJ();
+
+	@Test
+	public void givenFirstName_WhenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.firstName("Prashant");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenFirstName_WhenNotProper_ShouldReturnFlase() {
+		boolean result = userRegistration.firstName("prashant");
+		Assert.assertEquals(false, result);
+	}
+}
